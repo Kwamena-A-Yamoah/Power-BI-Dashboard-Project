33db35ac6fafe36b6371ec6376e86aaee29bf59d
Kwamena Afful Yamoah <kay.yamoah10@gmail.com>
2024-09-15 20:56:03 +0000

    move codes

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ def load_data():
     # Cache the data loading function
     @st.cache_data(persist=True)
     def get_data():
-        df = pd.read_csv(r'C:\Users\Pc\Desktop\Data analysis\Azubi Africa\Career Accelerator\Capstone\Power-BI-Dashboard-Project\Data\Cleaned data\cleaned_data.csv')
+        df = pd.read_csv(r'Data\Cleaned data\cleaned_data.csv')
         df = df.drop('Unnamed: 0', axis= 1, errors= ' ignore')
         return df
     return get_data()
